21c3e81fe33ab9287d6f7fa6ea9e30403500777d
l0o0gy <minamahone9620@gmail.com>
2024-05-18 03:01:51 +0300

    add changes to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,28 @@ const Card = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return (
+    <VStack 
+    color='black'
+    backgroundColor='white'
+    cursor='pointer'
+    borderRadius='xl'
+    >
+      <Image src={imageSrc} borderRadius='xl'/>
+
+      <VStack spacing={4} p={4} alignItems="flex-start">
+        <HStack justfyContact='space-between'
+        alignItems='center'>
+          <Heading as='h3' size='md'>{title}</Heading>
+        </HStack>
+        <text color='#64748b' fontSize='lg'>{description}</text>
+        <HStack>
+          <p>See More</p>
+          <FontAwesomeIcon icon={faArrowRight} size='1x'/>
+        </HStack>
+      </VStack>
+    </VStack>
+  );
 };
 
 export default Card;
